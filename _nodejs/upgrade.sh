7d114e2b3d2d13889673b052fae56311892c589c
zouyongsheng <zouyongsheng@kuaishou.com>
2022-12-15 21:43:35 +0800

    feat: 优化脚本

--- _nodejs/upgrade.sh
+++ _nodejs/upgrade.sh
@@ -1,3 +1,8 @@
+configPath="./config"
+if [ ! -d "$configPath" ]; then
+  echo "执行升级之前，请求复制原文件夹下的config文件夹到本目录下！"
+  exit
+fi
 npm install --registry=https://registry.npm.taobao.org
 npx pm2 kill
 npx pm2 start index.js
