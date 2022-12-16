f53d9a843982ad88ebf21360bec5e21437e9bfbd
zouyongsheng <zouyongsheng@kuaishou.com>
2022-12-16 12:30:02 +0800

    feat: 升级脚本

--- _nodejs/upgrade.sh
+++ _nodejs/upgrade.sh
@@ -1,11 +1,11 @@
-exePath=`ps aux | grep node | grep '/mybricks/index.js' | awk '{print $NF}'`
+exePath=`ps aux | grep node | grep '/mybricks' | awk '{print $NF}'`
 rawdir=`dirname $exePath`
 rawConfigPath="$rawdir/config"
 currentConfigPath="./config"
 if [ -d "$rawConfigPath" ]; then
-  cp -a $configPath './'
+  cp -r $rawConfigPath './'
   echo "配置同步成功，即将执行升级操作"
-elif [ -d "$currentConfigPath" ]
+elif [ -d "$currentConfigPath" ]; then
   echo "当前目录下已存在配置，即将执行升级操作"
 else
   echo "升级失败，请手动复制原文件夹下的config文件夹到本目录下！"
