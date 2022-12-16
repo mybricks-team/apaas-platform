bc2b19c2131d9485125924a8ea226274207b6b52
zouyongsheng <zouyongsheng@kuaishou.com>
2022-12-16 12:08:57 +0800

    feat: 升级脚本

--- _nodejs/upgrade.sh
+++ _nodejs/upgrade.sh
@@ -1,6 +1,14 @@
-configPath="./config"
-if [ ! -d "$configPath" ]; then
-  echo "执行升级之前，请求复制原文件夹下的config文件夹到本目录下！"
+exePath=`ps aux | grep node | grep '/mybricks/index.js' | awk '{print $NF}'`
+rawdir=`dirname $exePath`
+rawConfigPath="$rawdir/config"
+currentConfigPath="./config"
+if [ -d "$rawConfigPath" ]; then
+  cp -a $configPath './'
+  echo "配置同步成功，即将执行升级操作"
+elif [ -d "$currentConfigPath" ]
+  echo "当前目录下已存在配置，即将执行升级操作"
+else
+  echo "升级失败，请手动复制原文件夹下的config文件夹到本目录下！"
   exit
 fi
 npm install --registry=https://registry.npm.taobao.org
