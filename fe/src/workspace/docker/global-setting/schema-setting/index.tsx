1443a81e905cba6e7068b8953e0e0fa5f021f3c3
liubiao03 <cocolbell@163.com>
2022-12-19 15:54:54 +0800

    feat: add compilework to formitem

--- fe/src/workspace/docker/global-setting/schema-setting/index.tsx
+++ fe/src/workspace/docker/global-setting/schema-setting/index.tsx
@@ -1,7 +1,6 @@
-import React, { CSSProperties, useCallback, useEffect, useMemo, useState } from 'react'
+import React, { CSSProperties, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Form, Select, Input, Button, message } from 'antd'
-import CompileWorkflows from './form-items/compile-workflows'
-// import CompileWorkflow from './form-items/compile-workflow'
+import CompileWorkflow from './form-items/compile-workflow'
 
 // @ts-ignore
 import styles from './index.less'
@@ -25,75 +24,50 @@ interface SchemaSettingProps {
   style: CSSProperties,
 }
 
-const simpleCopyObject = target => {
-  let res = {}
-  try {
-    res = JSON.parse(JSON.stringify(target ?? {}))
-  } catch (error) {
-    res = {}
-  }
-  return res
-}
-
 export default ({ initialValues, schema = [], onSubmit, style }: SchemaSettingProps) => {
   const [form] = Form.useForm()
-
-  const compileWorkflowsKeyName = useMemo(() => {
-    return schema.find(t => t.type === 'compileWorkflows')?.id || '';
-  }, [schema])
-
-  useEffect(() => {
-    if (!!!compileWorkflowsKeyName) {
-      form?.setFieldsValue?.(initialValues)
-      return
-    }
-    const newValues = simpleCopyObject(initialValues)
-    newValues[compileWorkflowsKeyName] = Object.keys(initialValues?.[compileWorkflowsKeyName] ?? {}).map((keyName) => {
-      return {
-        name: decodeURI(keyName),
-        ...(initialValues?.[compileWorkflowsKeyName]?.[keyName] ?? {}),
-      }
-    })
-    form?.setFieldsValue?.(newValues)
-  }, [initialValues, compileWorkflowsKeyName])
+  const subForms = useRef([])
 
   const handleSubmit = useCallback(() => {
-    form?.validateFields().then((values) => {
-      const newVals = simpleCopyObject(values);
-      if (newVals?.[compileWorkflowsKeyName]) {
-        const oldArray = JSON.parse(JSON.stringify(newVals[compileWorkflowsKeyName])) 
-        delete newVals[compileWorkflowsKeyName]
-        oldArray.forEach((env, index) => {
-          const { name, ...others } = env
-          if (!newVals[compileWorkflowsKeyName]) {
-            newVals[compileWorkflowsKeyName] = {}
-          }
-          newVals[compileWorkflowsKeyName][encodeURI(name)] = { ...others, index }
-        })
-      }
-      typeof onSubmit === 'function' && onSubmit(newVals)
+    ;(async() => {
+      const values = await form?.validateFields();
+      await Promise.all(subForms.current.map(subForm => subForm?.validateFields?.()))
+      onSubmit?.(values)
+    })().catch(e => {
+      // message.error(e?.message || e.msg || '提交失败')
+      console.warn(e)
     })
-  }, [onSubmit, compileWorkflowsKeyName])
-
-  /** TODO临时代码，这里只能单例了 */
-  const compileWorkflowsValue = Form.useWatch(compileWorkflowsKeyName, form) || [];
+  }, [onSubmit])
 
   return (
     <div className={styles.schemaSetting} style={style}>
       <Form
         form={form}
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 20 }}
+        // labelCol={{ span: 4 }}
+        // wrapperCol={{ span: 20 }}
+        initialValues={initialValues}
+        layout="vertical"
         labelAlign="left"
         autoComplete="off"
         className={styles.form}
       >
         {
           schema.map(setting => {
-            if (setting.type === 'compileWorkflows') {
-              return <CompileWorkflows title={setting.title} name={setting.id} form={form} value={compileWorkflowsValue} />
-            }
             switch (true) {
+              case setting.type === 'compileWorkflows': {
+                return (
+                  <Form.Item
+                    label={setting.title}
+                    name={setting.id}
+                    required={setting.required}
+                    rules={setting.required ? [{ required: true, message: `请设置${setting.title}` }] : []}
+                  >
+                    <CompileWorkflow {...(setting?.props ?? {})} onInit={(_form) => {
+                      subForms.current.push(_form)
+                    }} />
+                  </Form.Item>
+                )
+              }
               case setting.type === 'input': {
                 return (
                   <Form.Item
