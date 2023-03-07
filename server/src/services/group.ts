3075d434cb13ea86b1943e7a1ee24cfdf1c78d87
leon-js <42924541+leon-js@users.noreply.github.com>
2023-03-07 11:29:12 +0800

    feat:协作组人员配置权限

--- server/src/services/group.ts
+++ server/src/services/group.ts
@@ -116,7 +116,6 @@ export default class UserGroupService {
     let data = []
 
     if (userGroupRelations.length) {
-      console.log(userGroupRelations)
       data = await this.userGroupDao.queryByIds({ids: userGroupRelations.map((item) => item.userGroupId)})
     }
 
@@ -141,9 +140,7 @@ export default class UserGroupService {
     } else {
       return {
         code: -1,
-        data: {
-          message: '仅创建人可删除'
-        }
+        message: '仅创建人可删除'
       }
     }
   }
@@ -200,10 +197,9 @@ export default class UserGroupService {
   @Get('/userGroup/getGroupInfoByGroupId')
   async getGroupInfoByGroupId(@Query() query) {
     const { id, pageIndex, pageSize, userId } = query
-  
     const [group, groupUsers, userTotal, userGroupRelation] = await Promise.all([
       await this.userGroupDao.queryById({id}),
-      await this.userGroupRelation.queryByUserGroupId({userGroupId: id, limit: pageSize, offset: pageSize * pageIndex}),
+      await this.userGroupRelation.queryByUserGroupId({userGroupId: id, limit: Number(pageSize), offset: Number(pageSize) * (Number(pageIndex) - 1)}),
       await this.userGroupRelation.queryUserTotalByUserGroupId({userGroupId: id}),
       await this.userGroupRelation.queryByUserIdAndUserGroupId({userId, userGroupId: id})
     ])
@@ -218,6 +214,68 @@ export default class UserGroupService {
           const { role_description, ...other } = user
           return {...other, roleDescription: role_description}
         })
+      }
+    }
+  }
+
+  @Get('/userGroup/getGroupUsersByGroupId')
+  async getGroupUsersByGroupId(@Query() query) {
+    const { id, pageIndex, pageSize } = query
+    const [users, total] = await Promise.all([
+      await this.userGroupRelation.queryByUserGroupId({userGroupId: id, limit: Number(pageSize), offset: Number(pageSize) * (Number(pageIndex) - 1)}),
+      await this.userGroupRelation.queryUserTotalByUserGroupId({userGroupId: id})
+    ])
+
+    return {
+      code: 1,
+      data: {
+        users: users.map((user: any) => {
+          const { role_description, ...other } = user
+          return {...other, roleDescription: role_description}
+        }),
+        total
+      }
+    }
+  }
+
+  @Post('/userGroup/updateUserGroupRelation')
+  async updateUserGroupRelation(@Body() body) {
+    const { id, userId, operatedUserId } = body
+    const roleDescription = Number(body.roleDescription)
+    const [user, userGroupRelation] = await Promise.all([
+      await this.userDao.queryByEmail({email: userId}),
+      await this.userGroupRelation.queryByUserIdAndUserGroupId({userId, userGroupId: id})
+    ])
+
+    if (userGroupRelation?.roleDescription === 1) {
+      const params: any = {
+        updatorId: userId,
+        updatorName: user.name || userId,
+        userGroupId: id,
+        userId: operatedUserId,
+      }
+      switch (true) {
+        case roleDescription === 1:
+        case roleDescription === 2:
+        case roleDescription === 3:
+          params.roleDescription = roleDescription
+          break
+        case roleDescription === -1:
+          params.status = -1
+          break
+        default:
+          break
+      }
+
+      await this.userGroupRelation.update(params)
+      return {
+        code: 1,
+        data: {}
+      }
+    } else {
+      return {
+        code: -1,
+        message: '无权操作'
       }
     }
   }
