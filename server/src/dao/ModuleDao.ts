3c795d54c41dccf7c60fcb7caa770fbf848d9f67
liubiao03 <cocolbell@163.com>
2023-03-11 18:40:24 +0800

    feat: support module publish

---
+++ server/src/dao/ModuleDao.ts
@@ -0,0 +1,110 @@
+import { Column, DOBase, Mapping } from "@mybricks/rocker-dao";
+import * as moment from "dayjs";
+
+export class ModuleDO {
+  @Column
+  id: number;
+
+  @Column("name")
+  name: string;
+
+  @Column("description")
+  description: string;
+
+  @Column("origin_file_id")
+  originFileId: number;
+
+  @Column("version")
+  version: string;
+
+  @Column("status")
+  status: number;
+
+  @Column("creator_id")
+  creatorId: string;
+
+  @Column("creator_name")
+  creatorName: string;
+
+  @Column("create_time")
+  createTime(a) {
+    return moment(a).format("YYYY-MM-DD HH:mm:ss");
+  }
+}
+
+export default class ModuleDao extends DOBase {
+  // @Mapping(ConfigDO)
+  // public async getConfig(params: { namespace: string[] }) {
+  //   return await this.exe<ConfigDO[]>("apaas_config:getConfigByNamespace", {
+  //     ...params,
+  //   });
+  // }
+
+  @Mapping(ModuleDO)
+  async getLatestPubByFileId(params: { fileId: number }) {
+    return await this.exe<ModuleDO[]>('apaas_module_info:getLatestPubByFileId', {
+      ...params,
+    })
+  }
+
+  @Mapping(ModuleDO)
+  public async queryByFileId(params: {
+    fileId: number;
+    limit?: number;
+    offset?: number;
+  }) {
+    params = Object.assign({}, params)
+
+    if (!params.limit) {
+      params.limit = 1
+    } else {
+      params.limit = Number(params.limit)
+    }
+    if (!params.offset) {
+      params.offset = 0
+    } else {
+      params.offset = Number(params.offset)
+    }
+    return await this.exe<ModuleDO[]>(
+      'apaas_module_info:queryByFileId',
+      params
+    )
+  }
+
+  @Mapping(ModuleDO)
+  async getModules(params: {
+    id: number
+  }) {
+    return await this.exe<ModuleDO[]>('apaas_module_info:getModules', {
+      ...params,
+    })
+  }
+
+  async create(params: {
+    id: number;
+    name: string;
+    description: string
+    originFileId: number
+    version: string
+    creatorId: string
+    creatorName: string
+  }) {
+    const result = await this.exe<{ insertId: number }>("apaas_module_info:create", {
+      ...params,
+      createTime: Date.now(),
+    });
+    return { id: result.insertId };
+  }
+
+  // async update(params: {
+  //   updatorId: string;
+  //   updatorName: string;
+  //   config: string;
+  //   namespace: string;
+  // }) {
+  //   await this.exe<{ insertId: number }>("apaas_config:update", {
+  //     ...params,
+  //     updateTime: new Date().getTime(),
+  //   });
+  // }
+}
---
+++ server/src/dao/ModulePubDao.ts
@@ -0,0 +1,42 @@
+import { Column, DOBase, Mapping } from "@mybricks/rocker-dao";
+import * as moment from "dayjs";
+
+export class ModulePubDO {
+  @Column
+  id: number;
+
+  @Column("version")
+  version: string;
+
+  @Column("status")
+  status: number;
+
+  @Column("creator_id")
+  creatorId: string;
+
+  @Column("creator_name")
+  creatorName: string;
+
+  @Column("create_time")
+  createTime(a) {
+    return moment(a).format("YYYY-MM-DD HH:mm:ss");
+  }
+}
+
+export default class ModulePubDao extends DOBase {
+  async batchCreate(params: {
+    moduleId: number,
+    pubContentList: any[],
+    version: string,
+    creatorId: string,
+    creatorName: string
+  }) {
+    const result = await this.exe<{ id }>('apaas_module_pub_info:batchCreate', {
+      ...params,
+      createTime: new Date().getTime()
+    })
+    return {
+      id: result && result.insertId ? result.insertId : null
+    }
+  }
+}
