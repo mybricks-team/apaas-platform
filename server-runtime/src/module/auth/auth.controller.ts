02ea5b460b61ae7482417573380115412eb21709
zouyongsheng <zouyongsheng@kuaishou.com>
2023-03-29 21:29:03 +0800

    feat: 修复

---
+++ server-runtime/src/module/auth/auth.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Inject,
+  Query,
+  Param,
+  Request,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import AuthService from './auth.service';
+const path = require('path');
+const env = require('../../../env.js')
+const fs = require('fs');
+const { getConnection } = require("@mybricks/rocker-dao");
+const { SnowFlake } = require('gen-uniqueid');
+
+@Controller('/runtime/api/auth')
+export default class AuthController {
+  @Inject()
+  authService: AuthService;
+  
+  // runtime env
+  snowFlake: any
+  
+  constructor() {
+    this.snowFlake = new SnowFlake({ workerId: process.env.WorkerId == undefined ? 1 : process.env.WorkerId });
+  }
+
+  // 领域建模运行时(运行时)
+  @Post('/service/run')
+  async systemDomainRun(
+    @Body('serviceId') serviceId: string,
+    @Body('params') params: any,
+    @Body('fileId') fileId: number,
+    @Body('projectId') projectId: number
+  ) {
+    if (!serviceId) {
+      return {
+        code: -1,
+        msg: '缺少 serviceId',
+      };
+    }
+    let readyExePath;
+    try {
+      const readyExeTemplateFolderPath = projectId ? path.join(env.FILE_LOCAL_STORAGE_FOLDER, `/project/${projectId}/${fileId}`) : path.join(env.FILE_LOCAL_STORAGE_FOLDER, `/project/${fileId}`);
+      readyExePath = path.join(readyExeTemplateFolderPath, `${serviceId}.js`);
+      const { startExe } = require(readyExePath)
+      let res = await startExe(params || {}, {
+        dbConnection: await getConnection(),
+        snowFlake: this.snowFlake
+      })
+      return {
+        code: 1,
+        data: res
+      }
+    } catch (e) {
+      return {
+        code: -1,
+        msg: `${typeof e === 'string' ? e : e.message}`
+      }
+    }
+  }
+
+}
