0b0218a3c3e3031b259298bf857d9678c7d2c72d
zouyongsheng <zouyongsheng@kuaishou.com>
2023-04-13 10:10:08 +0800

    feat: 修复

--- server-runtime/src/App.module.ts
+++ server-runtime/src/App.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from '@nestjs/schedule';
 import DomainModule from "./module/domain/domain.module";
-import TaskModule from "./module/task/task.module";
 import ServerModule from "./module/server/server.module";
 import AuthModule from "./module/auth/auth.module";
 
@@ -10,7 +9,6 @@ import AuthModule from "./module/auth/auth.module";
     ScheduleModule.forRoot(),
     DomainModule,
     ServerModule,
-    TaskModule,
     AuthModule
   ],
   controllers: [
--- server-runtime/src/module/task/task.service.ts
+++ server-runtime/src/module/task/task.service.ts
@@ -1,25 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-const { getConnection } = require("@mybricks/rocker-dao");
 
 
 @Injectable()
-export default class TaskService {
-  @Cron(CronExpression.EVERY_HOUR)
-  async activateConnection() {
-    try {
-      const con = await getConnection()
-      con.query({
-        sql: 'select * from apaas_file where id = 1',
-        timeout: 10 * 1000,
-      }, {}, function (error, results) {
-        if (error) {
-          return
-        }
-        console.log('activate connection successfully!')
-      })
-    } catch (err) {
-      console.log('activate connection failed! --' + err.message)
-    }
-  }
-}
+export default class TaskService {}
