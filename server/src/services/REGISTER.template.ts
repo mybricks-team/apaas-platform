93a8306b585ce5982fad3c0826e1718eea22a5c0
liulei11 <liulei11@kuaishou.com>
2023-05-25 16:43:13 +0800

    feat: 系统表字段补充

--- server/src/services/REGISTER.template.ts
+++ server/src/services/REGISTER.template.ts
@@ -16,9 +16,10 @@ function startExe(obj, { dbConnection, genUniqueId }) {
 				reject(Error('电话号码已被使用'));
 			}
 			const getValue = (key) => (obj[key] === undefined || obj[key] === null) ? 'null' : `'${obj[key]}'`;
+			const time = Date.now();
 	    await _execSQL(
-				`INSERT INTO D_${projectId}_系统用户_VIEW (id, _STATUS_DELETED, 用户名, 电话号码, 邮箱, 密码, weixinOpenid, weixinUnionid)
- 							VALUES (${genUniqueId()}, 0, ${getValue('username')}, ${getValue('phone')}, ${getValue('email')}, ${getValue('password')}, ${getValue('weixinOpenid')}, ${getValue('weixinUnionid')});`,
+				`INSERT INTO D_${projectId}_系统用户_VIEW (id, _STATUS_DELETED, _CREATE_TIME, _UPDATE_TIME, 注册日期, 用户名, 电话号码, 邮箱, 密码, weixinOpenid, weixinUnionid)
+ 							VALUES (${genUniqueId()}, 0, ${time}, ${time}, ${time}, ${getValue('username')}, ${getValue('phone')}, ${getValue('email')}, ${getValue('password')}, ${getValue('weixinOpenid')}, ${getValue('weixinUnionid')});`,
 		    { args: obj }
 	    );
 	    const userRes = await _execSQL(`SELECT id, 用户名, 电话号码, 邮箱 FROM D_${projectId}_系统用户_VIEW WHERE _STATUS_DELETED = 0 AND 电话号码 = '${phone}' AND 密码 = '${password}' ORDER BY id DESC LIMIT 1;`, { args: obj });
