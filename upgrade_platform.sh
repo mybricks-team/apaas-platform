949eccffd4f28f9ae5ed49f4161e7f4c8c1b60cb
zouyongsheng <zouyongsheng@kuaishou.com>
2023-06-01 17:27:34 +0800

    feat: 修复

--- upgrade_platform.sh
+++ upgrade_platform.sh
@@ -47,6 +47,14 @@ then
 fi
 echo "覆盖完毕"
 
+echo "开始执行安装依赖操作"
+cd $tmpFolderBase
+cd "../server"
+npm i --registry=https://registry.npm.taobao.org 
+cd "../server-runtime"
+npm i --registry=https://registry.npm.taobao.org 
+echo "依赖安装完毕"
+
 # echo "开始重启服务"
 # npx pm2 reload index
 # npx pm2 reload index_flow
