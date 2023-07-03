e09ebbe5a04af6ecfb46f3515f96b456e59fb838
zouyongsheng <zouyongsheng@kuaishou.com>
2023-07-03 16:42:41 +0800

    feat: 更新启动方式

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "index",
       script: "./index.js",
       instances: 1,
-      exec_mode: "fork",
+      exec_mode: "cluster",
       watch: false,
       env: {
         // "MYBRICKS_PLATFORM_ADDRESS": "http://block.yunzhiyuan100.com/"
