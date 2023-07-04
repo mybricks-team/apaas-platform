89af8804107334792f7304e15975a1e004eabe20
liulei11 <liulei11@kuaishou.com>
2023-07-04 17:17:24 +0800

    feat: 登录获取权限

--- server/src/services/LOGIN.template.ts
+++ server/src/services/LOGIN.template.ts
@@ -8,14 +8,21 @@ function startExe(obj, { dbConnection }) {
   };
   // userId为主键
   const { projectId, username, password } = obj
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
       const sql = `SELECT id, 用户名 FROM D_${projectId}_系统用户_VIEW WHERE _STATUS_DELETED = 0 AND 用户名 = '${username}' AND 密码 = '${password}' ORDER BY id DESC LIMIT 1;`;
-      _execSQL(sql, { args: obj }).then(res => {
-        resolve(res[0]);
-      })
-    }
-    catch(error) {
+      const res = await _execSQL(sql, { args: obj });
+      const user = res[0];
+
+      if (!user) {
+        resolve(undefined);
+      } else {
+        const authRes = await _execSQL(`SELECT 角色名 FROM D_${projectId}_系统权限关系_VIEW LEFT JOIN D_${projectId}_系统权限_VIEW ON D_${projectId}_系统权限关系_VIEW.系统权限 = D_${projectId}_系统权限_VIEW.id WHERE 系统用户 = ${user.id} ORDER BY id DESC LIMIT 1;`, { args: obj });
+
+        user.角色权限 = authRes[0];
+        resolve(user);
+      }
+    } catch(error) {
       console.log('【执行SQL】：执行沙箱内sql出错: ' + error?.message);
       reject(error)
     }
