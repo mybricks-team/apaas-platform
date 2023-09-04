d81cc693c309c9de1e0c33f3cc236e5d0d8e81d6
andyzys <ml_zys@126.com>
2023-09-04 12:04:11 +0800

    feat: 暂时注释无用代码

--- page/src/app/AppCtx.ts
+++ page/src/app/AppCtx.ts
@@ -145,12 +145,10 @@ export default class AppCtx {
     /** 平台默认,搭建应用 */
     const DesignAPPS: Array<T_App> = [
       // 平台特殊应用特殊处理
-      
     ];
 
     /** 平台默认,侧边栏应用 */
     const DockerAPPS: Array<any> = [
-      /** TODO:暂时隐藏，目前应用也没有分享按钮 */
       {
         title: '大家的分享',
         description: '大家的分享',
@@ -237,17 +235,18 @@ export default class AppCtx {
       this.FolderAPPS = this.FolderAPPS.filter((app) => {
         return SHOW_FOLDERS_MAP[app.extName]
       })
-      if(this.user?.role >= 3) {
-        // @ts-ignore
-        this.FolderAPPS.push({
-          title: '项目文件夹',
-          description: '通过项目的方式管理文件',
-          type: 'user',
-          extName: 'folder-project',
-          namespace: 'mybricks-folder-project',
-          icon: FolderProject
-        })
-      }
+      // console.log('111', this.user)
+      // if(this.user?.role >= 3) {
+      //   // @ts-ignore
+      //   this.FolderAPPS.push({
+      //     title: '项目文件夹',
+      //     description: '通过项目的方式管理文件',
+      //     type: 'user',
+      //     extName: 'folder-project',
+      //     namespace: 'mybricks-folder-project',
+      //     icon: FolderProject
+      //   })
+      // }
     } else {
       // 搭建应用
       this.DesignAPPS = DesignAPPS;
