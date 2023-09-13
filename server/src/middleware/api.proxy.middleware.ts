654ee6a0c4acd04ae1b7fd7ffbb0ba6e4d44792e
Leo <liulei.leo306@gmail.com>
2023-09-13 11:54:45 +0800

    feat: 修复代理 get  参数丢失

--- server/src/middleware/api.proxy.middleware.ts
+++ server/src/middleware/api.proxy.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request } from "express";
 const proxy = require('express-http-proxy');
+const URL = require('url');
 
 interface Option {
   proxyTarget?: string;
@@ -8,7 +9,7 @@ interface Option {
 export function apiProxy(option: Option = {}) {
   return async (req: Request, res, next: NextFunction) => {
     try {
-      const url = req.headers['X-Target-Url'] || req.headers['x-target-url'];
+      let url = req.headers['X-Target-Url'] || req.headers['x-target-url'];
       const regex = /(?:https?:\/\/)?(?:www\.)?([^\/]+)/;
       const host = url?.match?.(regex)?.[1];
 
@@ -23,7 +24,23 @@ export function apiProxy(option: Option = {}) {
       req.headers.origin = origin;
       req.headers.host = host;
 
-      return proxy(url, { limit: '100mb' })(req, res, next);
+      return proxy(url, {
+        limit: '100mb',
+        timeout: 10 * 1000,
+        proxyReqPathResolver: req => {
+          const parse = URL.parse(req.url, true);
+          parse.query = { ...(parse.query || {}), ...req.query };
+          let url = req.url.split('?')[0];
+
+          if (Object.keys(parse.query).length) {
+            Object.keys(req.query).forEach(key => {
+              url += `${url.includes('?') ? '&' : '?'}${key}=${req.query[key]}`;
+            });
+          }
+
+          return url;
+        }
+      })(req, res, next);
     } catch (e) {
       return next();
     }
