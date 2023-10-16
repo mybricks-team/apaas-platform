02cac9999c3cfb3227ee7e8f4d1ee0fbc623a746
andyzys <ml_zys@126.com>
2023-10-16 19:46:17 +0800

    feat: 更新版本

--- server/_deployment/install_docker.js
+++ server/_deployment/install_docker.js
@@ -15,7 +15,7 @@ function mergeToApplication() {
         },
         {
           "type": "oss",
-          "version": "1.1.60",
+          "version": "1.1.63",
           "namespace": "mybricks-app-pcspa",
           "path": "asset-center/asset/app/mybricks-app-pcspa/1.1.35/mybricks-app-pcspa.zip"
         },
