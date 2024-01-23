0948d78906e4701f067eb56f5d9b405fd43766a3
wufan12 <wufan12@kuaishou.com>
2024-01-23 17:37:11 +0800

    fix: 优化

--- server/src/middleware/log.middleware.ts
+++ server/src/middleware/log.middleware.ts
@@ -9,28 +9,25 @@ export function runtimeLogger(option: Option = {}) {
   const { appNamespaceList = [] } = option;
 
   return async (req: Request, res, next: NextFunction) => {
-    let params;
     let formattedParams = req.query;
     try {
       formattedParams = formatBodyOrParamsData(req.query);
     } catch (err) {
       formattedParams = req.query;
     }
-    params = JSON.stringify(formattedParams || null);
     const application = appNamespaceList.find(namespace => req.path?.startsWith?.(`/${namespace}`)) || 'platform';
 
     if (req.method !== 'GET' && req.method !== 'DELETE') {
-      let formattedData = req.body;
+      formattedParams = req.body;
       try {
-        formattedData = formatBodyOrParamsData(req.body);
+        formattedParams = formatBodyOrParamsData(req.body);
       } catch (err) {
-        formattedData = req.body;
+        formattedParams = req.body;
       }
-      params = JSON.stringify(formattedData || null);
     }
 
     res.on('close', () => {
-      Logger.info(`[application: ${application}] [timestamp: ${new Date().toLocaleString()}] [code: ${res.statusCode}] [method: ${req.method}] [refer: ${req.headers.referer}] [userAgent: ${req.headers['user-agent']}] [params: ${params}] [path: ${req.path}] [ip: ${req.ip}]`);
+      Logger.info(`[application: ${application}] [timestamp: ${new Date().toLocaleString()}] [code: ${res.statusCode}] [method: ${req.method}] [refer: ${req.headers.referer}] [userAgent: ${req.headers['user-agent']}] [params: ${JSON.stringify(formattedParams || null)}] [path: ${req.path}] [ip: ${req.ip}]`);
     });
     next();
   };
