acc4df4a450a67a30f57850b6497c8a40b60645c
Leo <liulei.leo306@gmail.com>
2024-04-08 17:56:32 +0800

    feat: 更新默认 sql

--- server/_deployment/sql/apaas_user_log.sql
+++ server/_deployment/sql/apaas_user_log.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `apaas_user_log` (
   `type` int NOT NULL COMMENT 'type',
   `user_id` bigint NOT NULL COMMENT 'user_id',
   `user_email` varchar(256) NOT NULL COMMENT 'user_email',
+  `relation_token` bigint DEFAULT NULL COMMENT '日志关联的id',
   `log_content` varchar(512) NOT NULL COMMENT 'log_content',
   `create_time` bigint NOT NULL COMMENT 'create_time',
   PRIMARY KEY (`id`)
